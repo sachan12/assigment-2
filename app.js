3bde921da1d250b0d0729e3a95f74b2259a5b10c
sachan12 <sachinnanda427@gmail.com>
2024-03-06 01:26:22 -0800

    add files

---
+++ app.js
@@ -0,0 +1,177 @@
+// CHAPTER-04
+// Question-01
+  var a,b, c ;
+  console.log(a,b,c);
+
+// Question-02
+document.write("<h1>"+ "LEGAL VARIABLES :" +"</h1>")
+document.write("<ul>"+ "<li>"+ "firstName"+ "</li>" + "<li>"+ "result"+ "</li>" + "<li>"+ "answer"+ "</li>"+"<li>"+ "$price"+ "</li>"+"<li>"+ "Bussness"+ "</li>"+"</ul>")
+document.write("<h1>"+ "ILLEGAL VARIABLES :" +"</h1>")
+document.write("<ul>"+ "<li>"+ "var"+ "</li>" + "<li>"+ "console"+ "</li>" + "<li>"+ "First-Name"+ "</li>"+"<li>"+ "5555"+ "</li>"+"<li>"+ "---java"+ "</li>"+"</ul>")
+// Question-03
+document.write("<h1>"+ "Rules For Naming Js Variables :" +"</h1>")
+document.write("<ol>"+ "<li>"+ "Variabls names can only.numbers,$ and - .For Example $ my-1st variable "+ "</li>" + "<li>"+ "Variable must begin with a letter, $ or -. For example : $name, -name or name "+ "</li>" + "<li>"+ "Variables names are case sensitive "+ "</li>"+"<li>" +"Variables names should not be JS keywords."+"</li>" +"</ol>")
+
+// CHAPTER-05
+// Question-01
+var a = 3;
+var b = 5;
+var result = a + b;
+console.log(result);
+
+// Question-02
+var a = 5;
+var b = 3;
+var result = a - b;
+console.log(result);
+
+var a = 5;
+var b = 3;
+var result = a * b;
+console.log(result);
+
+var a = 5;
+var b = 3;
+var result = a / b;
+console.log(result);
+
+var a = 5;
+var b = 3;
+var result = a % b;
+console.log(result)
+
+// Question-03
+var a = 5
+console.log (++a)
+a= a+7
+console.log(a)
+a= a - 12;
+console.log(a)
+console.log(a/3);
+
+// Question-04
+var ticketPrice = 600;
+var buyingTickets = 5;
+var result = ticketPrice * buyingTickets;
+console.log (result)
+
+// Question-05
+ var table = 4*1
+ console.log(table);
+ var table = 4*2
+ console.log(table);
+ var table = 4*3
+ console.log(table);
+ var table = 4*4
+ console.log(table);
+ var table = 4*5
+ console.log(table);
+ var table = 4*6
+ console.log(table);
+ var table = 4*7
+ console.log(table);
+ var table = 4*8
+ console.log(table);
+ var table = 4*9
+ console.log(table);
+ var table = 4*10
+ console.log(table);
+
+
+// Question-06
+var xyz = ( 77-32) * 5/9 ;
+document.write(xyz ,"<br>");
+var abc = (25 * 9/5) + 32;
+document.write(abc ,"<br>");
+// Question-07
+var item1 = 650
+var quantityitem1 = 3
+var item2 = 100
+var quantityitem2 = 7
+var  shippingcharges = 100
+
+var totalCost = item1 * quantityitem1 + shippingcharges + item2 * quantityitem2
+console.log(totalCost);
+
+// Question-08
+var totalMarks = 980;
+var marksObtained = 804;
+var Percentage = 804/980*100;
+console.log (Percentage);
+
+// Question-09
+
+var dollar = 4
+var RS = 280
+var result = 4*280
+console.log(result)
+
+var riyal = 25
+var RS = 80
+var result = 25*80
+console.log(result)
+
+// Question-010
+
+var num = 5;
+console.log(num);
+num = num+5;
+console.log(num);
+num= num * 7;
+console.log(num);
+num = num / 6;
+console.log(num);
+
+
+
+// Question-11
+var currentyear = 2024
+var birthdayyear = 2006
+var age = 2024-2006
+console.log(age);
+
+
+
+// Question-12
+ var radius = 2*3.142*20
+ document.write(radius ,"<br>");
+ var area = 3.142 * 400 
+ document.write(area ,"<br>");
+
+// Question-13
+var favoritesnack = "choclatechip"
+var currentage  = 15
+var  estimatedmaximumage=65
+var amountofsnacksperday = 3
+var calculate = ( estimatedmaximumage - currentage  ) * amountofsnacksperday
+console.log(calculate);
+
+// CHAPTER-06
+// Question-01
+var a =10
+document.write(a ,"<br>")
+++a
+document.write(a ,"<br>")
+a++
+document.write(a ,"<br>")
+--a
+document.write(a ,"<br>")
+a--
+document.write(a ,"<br>")
+// Question-02
+var a=2 , b=1
+--a
+document.write(a ,"<br>")
+
+--a - --b
+document.write(a,b ,"<br>")
+
+--a - --b + ++b;
+document.write(a,b ,"<br>")
+
+--a - --b + ++b + b--;
+document.write(a,b ,"<br>")
+
+// Question-03
+document.write("<input type='text' + placeholder='Enter your name' + maxlength='10' + size='10'  />" ,"<br>")
+
